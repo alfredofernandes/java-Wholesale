7256378844deb7679aca0de774a1d3b3fdcb26fd
Juliana Lacerda <juli.m.lima@gmail.com>
2017-07-20 10:50:45 -0400

    - create classes manager files

---
+++ src/Service/CategoryManager.java
@@ -0,0 +1,58 @@
+package Service;
+
+import Product.Category;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CategoryManager {
+
+    final private String FILENAME = "category.txt";
+    private File file;
+    private Scanner readFile;
+    private PrintWriter writeFile;
+
+    public CategoryManager() {
+        this.file = new File(FILENAME);
+        try {
+            this.readFile = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Category> loadFile()  {
+
+        ArrayList<Category> categories = new ArrayList<Category>();
+
+        while(this.readFile.hasNext())
+        {
+            String line = this.readFile.nextLine();
+            String[] column = line.split("/");
+
+            Category category = new Category(column[1]);
+            categories.add(category);
+        }
+
+        this.readFile.close();
+
+        return categories;
+    }
+
+    public void addCategory(Category category) {
+
+        try {
+            this.writeFile = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String data = category.getCategoryId() + "/" + category.getName();
+
+        this.writeFile.println(data);
+        this.writeFile.close();
+    }
+}
